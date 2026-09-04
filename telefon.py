6cbbe812834e8535827ec3344f219e94b9914f9c
“kaylalorrainealessandra-ux” <kaylalorrainealessandra@gmail.com>
2026-09-04 15:10:57 +0200

    har brukt for løkke for å vise navn og nummer

--- telefon.py
+++ telefon.py
@@ -1,15 +1,20 @@
 telefonbok = []
 
 person1 = {
-    "Sverre": "46576760"
+    "navn": "Sverre",
+    "nummer": "46576760"
 }
 person2 = {
-    "Andreas": "41226625"
+    "navn": "Andreas",
+    "nummer": 41226625
 }
 
 telefonbok.append(person1)
 telefonbok.append(person2)
 
 def vis_alle():
-    print(telefonbok)
+    for element in telefonbok: 
+        print(f"{element["navn"]} sitt nummer er: {element["nummer"]}")
+
 vis_alle()
+
